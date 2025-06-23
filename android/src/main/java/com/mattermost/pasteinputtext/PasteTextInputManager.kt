931bc8b36b939b71d428245d316397fb85860cd6
Samuel Newman <mozzius@protonmail.com>
2025-06-23 11:29:43 +0300

    fix(android): maybe fix kotlin build error

--- android/src/main/java/com/mattermost/pasteinputtext/PasteTextInputManager.kt
+++ android/src/main/java/com/mattermost/pasteinputtext/PasteTextInputManager.kt
@@ -53,12 +53,12 @@ class PasteTextInputManager(context: ReactApplicationContext) : ReactTextInputMa
     pasteInputEditText.setOnPasteListener(PasteInputListener(pasteInputEditText, reactContext.surfaceId), eventDispatcher)
   }
 
-  override fun getExportedCustomBubblingEventTypeConstants(): MutableMap<String, Any> {
-    val map = super.getExportedCustomBubblingEventTypeConstants()!!
-    map["onPaste"] = MapBuilder.of(
+  override fun getExportedCustomBubblingEventTypeConstants(): Map<String, Any>? {
+    val map = super.getExportedCustomBubblingEventTypeConstants() ?: mutableMapOf()
+    map.put("onPaste", MapBuilder.of(
       "phasedRegistrationNames",
       MapBuilder.of("bubbled", "onPaste")
-    )
+    ))
 
     return map
   }
